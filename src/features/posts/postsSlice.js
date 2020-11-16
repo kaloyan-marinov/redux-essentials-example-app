d835cf73870d482f0d9f94bdab9f686283e8c9f3
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-11-16 07:04:44 +0200

    update the Posts slice to add new post entries to the Redux store

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -8,7 +8,19 @@ const initialState = [
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {}
+  reducers: {
+    postAdded(state, action) {
+      // Since the posts slice _only_ knows about the data it's responsible for,
+      // the `state` argument will be the array of posts by itself,
+      // and not the entire Redux state object.
+      state.push(action.payload)
+    }
+  }
 })
+
+// When we write the `postAdded` reducer function,
+// `createSlice` will automatically generate an "action creator" function
+// with the same name.
+export const { postAdded } = postsSlice.actions
 
 export default postsSlice.reducer
