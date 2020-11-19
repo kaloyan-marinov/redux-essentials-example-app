0b85d517732a841f70e79746b16fe8ab3d33e5bd
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-11-19 19:35:10 +0200

    create a Notifications slice

---
+++ src/features/notifications/notificationsSlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+import { client } from '../../api/client'
+
+// The following is
+// "an async thunk ... which will retrieve a list of new notifications from the server".
+export const fetchNotifications = createAsyncThunk(
+  'notifications/fetchNotifications',
+  async (_, { getState }) => {
+    // In this case, we know that the list of notifications is in our Redux store state
+    const allNotifications = selectAllNotifications(getState())
+    // Since the array of notifications is sorted newest first,
+    // we can grab the latest one using array destructuring.
+    const [latestNotification] = allNotifications
+    const latestTimestamp = latestNotification ? latestNotification.date : ''
+    const response = await client.get(
+      `/fakeApi/notifications?since=${latestTimestamp}`
+    )
+    return response.notifications
+  }
+)
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState: [],
+  reducers: {},
+  extraReducers: {
+    [fetchNotifications.fulfilled]: (state, action) => {
+      // We know that we will be getting back an array of notifications,
+      // so we can pass them as separate arguments to:
+      state.push(...action.payload)
+      // Sort with newest first
+      // make sure that they're sorted ...
+      // ... just in case the server were to send them out of order
+      state.sort((notificationA, notificationB) =>
+        notificationB.date.localeCompare(notificationA.date)
+      )
+    }
+  }
+})
+
+export default notificationsSlice.reducer
+
+export const selectAllNotifications = state => state.notifications
