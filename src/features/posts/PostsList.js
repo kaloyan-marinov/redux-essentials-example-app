05b1b2a88c0543c8c5c3b4139483cb0e3dbd14ae
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-11-19 07:49:25 +0200

    display a "loading..." indicator in the UI (so that the user will know we're waiting for data)

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -19,6 +19,7 @@ export const PostsList = () => {
   const posts = useSelector(selectAllPosts)
 
   const postStatus = useSelector(state => state.posts.status)
+  const error = useSelector(state => state.posts.error)
 
   // Avoid fetching the posts
   // every time the PostsList component renders,
@@ -30,29 +31,37 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch])
 
-  const orderedPosts = posts
-    .slice()
-    .sort((postA, postB) => postB.date.localeCompare(postA.date))
+  let content
 
-  const renderedPosts = orderedPosts.map(post => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <div>
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date} />
-      </div>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <Link to={`/posts/${post.id}`} className="button muted-button">
-        View Post
-      </Link>
-      <ReactionButtons post={post} />
-    </article>
-  ))
+  if (postStatus === 'loading') {
+    return <div className="loader">Loading...</div>
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts
+      .slice()
+      .sort((postA, postB) => postB.date.localeCompare(postA.date))
+
+    content = orderedPosts.map(post => (
+      <article className="post-excerpt" key={post.id}>
+        <h3>{post.title}</h3>
+        <div>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
+        </div>
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+        <Link to={`/posts/${post.id}`} className="button muted-button">
+          View Post
+        </Link>
+        <ReactionButtons post={post} />
+      </article>
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
