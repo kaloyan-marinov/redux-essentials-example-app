d5f97e30d655546a58c50a5eeb5ba036d5bd3095
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-11-19 19:22:07 +0200

    Add UsersList and UserPage components

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ import { SinglePostPage } from './features/posts/SinglePostPage'
 
 import { EditPostForm } from './features/posts/EditPostForm'
 
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
+
 function App() {
   return (
     <Router>
@@ -34,6 +37,8 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
           <Redirect to="/" />
         </Switch>
       </div>
