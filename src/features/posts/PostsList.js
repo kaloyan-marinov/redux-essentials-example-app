151bb8e8926176e4994af8c2bfe88f8b2790e70d
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-11-21 09:20:35 +0200

    Improving Render Performance #2.1: prevent _all_ of the PostExcerpt components from re-rendering by wrapping the PostExcerpt component in React.memo()

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -14,7 +14,7 @@ import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchPosts } from './postsSlice'
 
-const PostExcerpt = ({ post }) => {
+let PostExcerpt = ({ post }) => {
   return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
@@ -31,6 +31,33 @@ const PostExcerpt = ({ post }) => {
     </article>
   )
 }
+
+/*
+Flaw #2:
+
+If we go back to our <PostsList> and try clicking a reaction button on one of the posts
+while capturing a React profiler trace,
+we'll see that not only did the <PostsList> and the updated <PostExcerpt> instance
+render, all of the <PostExcerpt> components rendered
+
+Why is that?...
+
+React's default behavior is that when a parent component renders,
+React will recursively render all child components inside of it!
+
+The immutable update of one post object also created a new `posts` array.
+Our <PostsList> had to re-render because the `posts` array was a new reference,
+so after it rendered,
+React continued downwards and re-rendered all of the <PostExcerpt> components too.
+
+There's a few different ways we could optimize this behavior in <PostsList>.
+
+Fix #2.1:
+
+wrap the <PostExcerpt> component in `React.memo()`, which will ensure that
+the component inside of it only re-renders if the props have actually changed.
+*/
+PostExcerpt = React.memo(PostExcerpt)
 
 export const PostsList = () => {
   const dispatch = useDispatch()
