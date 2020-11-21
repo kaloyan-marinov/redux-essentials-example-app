118552cfcc1f86efce57c85586b67cbcb436d538
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-11-21 08:40:06 +0200

    Improving Render Performance prep-for-#2: Extract a PostExcerpt component

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -14,6 +14,24 @@ import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchPosts } from './postsSlice'
 
+const PostExcerpt = ({ post }) => {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
+}
+
 export const PostsList = () => {
   const dispatch = useDispatch()
   const posts = useSelector(selectAllPosts)
@@ -41,18 +59,7 @@ export const PostsList = () => {
       .sort((postA, postB) => postB.date.localeCompare(postA.date))
 
     content = orderedPosts.map(post => (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <div>
-          <PostAuthor userId={post.user} />
-          <TimeAgo timestamp={post.date} />
-        </div>
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-        <Link to={`/posts/${post.id}`} className="button muted-button">
-          View Post
-        </Link>
-        <ReactionButtons post={post} />
-      </article>
+      <PostExcerpt key={post.id} post={post} />
     ))
   } else if (postStatus === 'failed') {
     content = <div>{error}</div>
